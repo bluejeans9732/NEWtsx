4cc60b5df421b786c07841e92eccc035fe1bf8ce
mc <dyd97wns@naver.com>
2023-07-20 19:37:33 +0900

    change generateStaticParams

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,15 +1,14 @@
+// detail.tsx
+
 import { connectDB } from '@/util/database';
 import { ObjectId } from 'bson';
 import { WithId, Collection } from 'mongodb';
-import Comment from './comment'
-
-import Notfound from './notfound';
-import { Post } from '@/util/types'
-
+import { Post } from '@/util/types';
+import Notfound  from './notfound';
+import Comment from './comment';
+import Link from 'next/link';
 
 export default async function Detail(props: { params: { id: string } }) {
-
-  
   const client = await connectDB();
   const db = client.db('practsx');
   const collection: Collection<WithId<Post>> = db.collection('post');
@@ -20,16 +19,35 @@ export default async function Detail(props: { params: { id: string } }) {
   if (post === null) {
     return <Notfound />;
   }
-  
+
   return (
     <div className='flex items-center justify-center h-screen bg-gradient-to-br from-gray-50 to-gray-100'>
       <div className='w-1/3 h-2/3 p-4 rounded-xl  shadow-xl bg-white'>
-        <h4 className='mb-3 text-center text-2xl text-cyan-900 font-bold'>상세페이지</h4>
-        <h4 className='mt-10 mb-3 ml-4 flex'><p className='mr-2 text-cyan-900 font-bold'>제목 : </p> {post?.title}</h4>
-        <div className='mb-10 ml-4 flex'><p className='mr-2 text-cyan-900 font-bold'>내용 : </p> {post?.content}</div>
+        <h4 className='mb-3 text-center text-2xl text-cyan-900 font-bold'>
+          상세페이지
+        </h4>
+        <h4 className='mt-10 mb-3 ml-4 flex'>
+          <p className='mr-2 text-cyan-900 font-bold'>제목 : </p> {post?.title}
+        </h4>
+        <div className='mb-10 ml-4 flex'>
+          <p className='mr-2 text-cyan-900 font-bold'>내용 : </p> {post?.content}
+        </div>
         <hr className='border-gray-300 my-3' />
-        <Comment _id={post._id.toString()}/>
+        <Comment _id={post._id.toString()} />
       </div>
     </div>
   );
+}
+
+export async function generateStaticParamsDetail() {
+  const client = await connectDB();
+  const db = client.db('practsx');
+  const collection: Collection<WithId<Post>> = db.collection('post');
+
+  const posts = await collection.find().toArray();
+  const postIds = posts.map((post) => post._id.toString());
+
+  client.close();
+
+  return postIds.map((id) => ({ params: { id } }));
 }
--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,10 +1,12 @@
+// edit.tsx
+
 import { connectDB } from '@/util/database';
 import { ObjectId } from 'bson';
 import { WithId, Collection } from 'mongodb';
-import { Post } from '@/util/types'
+import { Post } from '@/util/types';
+import Link from 'next/link';
 
 export default async function Edit(props: { params: { id: string } }) {
-  
   const client = await connectDB();
   const db = client.db('practsx');
   const collection: Collection<WithId<Post>> = db.collection('post');
@@ -14,23 +16,53 @@ export default async function Edit(props: { params: { id: string } }) {
 
   return (
     <div className='flex items-center justify-center h-screen bg-gradient-to-br from-gray-50 to-gray-100'>
-        <div className='w-1/3 h-[45%] p-4 rounded-xl  shadow-xl bg-white'>
-            <h4 className='mb-3 mt-8 text-center text-2xl text-cyan-900 font-bold'>글 수정</h4>
-            <form action="/api/post/edit" method="POST">
-                <div className='flex mt-10 mb-10'>   
-                    <div className='mr-2 text-cyan-900 font-bold p-2'>제목 : </div> 
-                    <input name="title" className='ml-4 w-[75%] flex border-2 p-2 focus:outline-none ' defaultValue={post?.title}></input>
-                </div>
-                <div  className='flex mt-10 mb-10'>
-                    <div className='mr-2 text-cyan-900 font-bold p-2'>내용 : </div> 
-                    <input name="content" className='mb-10 ml-4 w-[75%] flex p-2 border-2 focus:outline-none' defaultValue={post?.content}></input>
-                </div>
-                <input name="_id" className="mb-2 p-2 hidden"  defaultValue={post?._id.toString()}/>
-                <button type="submit" className="bg-emerald-300 rounded-lg mx-auto p-2 block">테스트 글수정 버튼</button>
-            </form>
-            
-            
-        </div>
+      <div className='w-1/3 h-[45%] p-4 rounded-xl  shadow-xl bg-white'>
+        <h4 className='mb-3 mt-8 text-center text-2xl text-cyan-900 font-bold'>
+          글 수정
+        </h4>
+        <form action="/api/post/edit" method="POST">
+          <div className='flex mt-10 mb-10'>
+            <div className='mr-2 text-cyan-900 font-bold p-2'>제목 : </div>
+            <input
+              name='title'
+              className='ml-4 w-[75%] flex border-2 p-2 focus:outline-none '
+              defaultValue={post?.title}
+            />
+          </div>
+          <div className='flex mt-10 mb-10'>
+            <div className='mr-2 text-cyan-900 font-bold p-2'>내용 : </div>
+            <input
+              name='content'
+              className='mb-10 ml-4 w-[75%] flex p-2 border-2 focus:outline-none'
+              defaultValue={post?.content}
+            />
+          </div>
+          <input
+            name='_id'
+            className='mb-2 p-2 hidden'
+            defaultValue={post?._id.toString()}
+          />
+          <button
+            type='submit'
+            className='bg-emerald-300 rounded-lg mx-auto p-2 block'
+          >
+            테스트 글수정 버튼
+          </button>
+        </form>
+      </div>
     </div>
   );
+}
+
+export async function generateStaticParamsEdit() {
+  const client = await connectDB();
+  const db = client.db('practsx');
+  const collection: Collection<WithId<Post>> = db.collection('post');
+
+  const posts = await collection.find().toArray();
+  const postIds = posts.map((post) => post._id.toString());
+
+  client.close();
+
+  return postIds.map((id) => ({ params: { id } }));
 }
